80b6ff483327b286c59f2204eac16de3628a4da2
Mamatha81230 <mamathareddy8123@gmail.com>
2024-01-10 17:24:46 +0530

    uvc

--- reduxclient/src/component/Navbar.js
+++ reduxclient/src/component/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ someValueFromRedux, dispatch }) => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            <strong>Home page </strong>
+            <strong>Home  </strong>
           </a>
         </div>
       </nav>
